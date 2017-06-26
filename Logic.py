68069ffe9fbd3ee4773873f1a4c5f4db5740b22d
Jerhone <fjcamillo.dev@gmail.com>
2017-06-27 07:40:03 +0800

    Added AND

---
+++ Logic.py
@@ -0,0 +1,13 @@
+import numpy as np
+from functools import reduce
+
+def perceptron(weight, bias, x):
+    logit = np.sum(np.add(np.dot(x, weight), bias))
+    return logit
+
+def main():
+    AND = {
+        'w1': ,
+        'w2': ,
+    }
+    
