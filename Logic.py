5f8a28f0c3ea83204bd505963e3bfa38dfdf0a60
Jerhone <fjcamillo.dev@gmail.com>
2017-06-27 08:00:15 +0800

    Added dataset and more logic

--- Logic.py
+++ Logic.py
@@ -1,13 +1,32 @@
 import numpy as np
 from functools import reduce
 
+
 def perceptron(weight, bias, x):
     logit = np.sum(np.add(np.dot(x, weight), bias))
     return logit
 
 def main():
-    AND = {
-        'w1': ,
-        'w2': ,
+    logic = {
+        'logic_and' : {
+            'w1': ,
+            'w2': ,
+        },
+        'logic_or': {
+            'w1': ,
+            'w2': ,
+        }
+        'logic_not': {
+            'w1': ,
+        }
     }
+    dataset = np.array(
+        [0,0],
+        [0,1],
+        [1,0],
+        [1,1]
+    )
     
+
+if __name__ == '__main__':
+    main()
