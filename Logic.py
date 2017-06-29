ee00d99fc90db3932bc65923637d18f6b85d439a
fjcamillo <fjcamillo.dev@gmail.com>
2017-06-29 23:40:30 +0800

    configuring and logic

--- Logic.py
+++ Logic.py
@@ -4,7 +4,8 @@ from functools import reduce
 
 def perceptron(weight, bias, x):
     model = np.sum(np.add(np.dot(x, weight), bias))
-    logit = 1/1+np.exp(model)
+    print(model)
+    logit = 1/(1+np.exp(-model))
     return logit
 
 def main():
@@ -31,9 +32,17 @@ def main():
 
     #Logic AND
     weights = np.array([logic['logic_and']['w1'],logic['logic_and']['w2']])
-    print(weights)
-    neuron = perceptron(weights, logic['bias'], dataset)
-    print(neuron)
+    first = perceptron(weights, logic['bias'], dataset[0])
+    second = perceptron(weights, logic['bias'], dataset[1])
+    third = perceptron(weights, logic['bias'], dataset[2])
+    fourth = perceptron(weights, logic['bias'], dataset[3])
+    print("""
+    Logic AND:
+    0 0 \t {}
+    0 1 \t {}
+    1 0 \t {}
+    1 1 \t {}
+    """.format(first, second, third, fourth))
     #Logic OR
     # weights = np.array([logic['logic_or']])
     # neuron = perceptron(weights, logic['bias'], dataset)
