9f879289135788013ea824bffb763fc763c8872c
fjcamillo.dev@gmail.com <fjcamillo.dev@gmail.com>
2019-10-28 00:46:04 +0800

    Rebuild perceptron class

--- neurallogic.py
+++ neurallogic.py
@@ -1,20 +1,51 @@
 import numpy as np
 
-class NeuralLogic:
+class Neural:
+    """Creates a Perceptron
+    
+    Returns:
+        [type] -- [description]
+    """
 
     def __init__(self, weights, bias, x):
-        self.weights = np.array(weights)
+        """[summary]
+        
+        Arguments:
+            weights {[type]} -- [description]
+            bias {[type]} -- [description]
+            x {[type]} -- [description]
+        """
+        self.weights = np.array(list(weights), dtype=np.double).reshape(len(weights), 1)
         self.x = np.array(x)
         self.bias = bias
+        self.logit = self.perceptron()
         
 
     def perceptron(self):
-        model = self.weights.transpose().dot(self.x) + self.bias
+        """[summary]
+        
+        Returns:
+            [type] -- [description]
+        """
+        model = self.x.dot(self.weights) + self.bias
         logit = 1/(1+np.exp(-model))
         return logit
 
     def __wshape__(self):
+        """[summary]
+        
+        Returns:
+            [type] -- [description]
+        """
         return self.weights.shape
 
     def __xshape__(self):
+        """[summary]
+        
+        Returns:
+            [type] -- [description]
+        """
         return self.x.shape
+    
+    def __converted__(self):
+        return np.round(self.logit)
