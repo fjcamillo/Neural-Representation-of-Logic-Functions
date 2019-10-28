6389a6b2d3b4cfe7b91b15960c17f7ea495651a1
fjcamillo <fjcamillo.dev@gmail.com>
2019-10-28 23:30:07 +0800

    Update README and class

--- README.md
+++ README.md
@@ -2,17 +2,29 @@
 
 # How to use
 
-```python
-weights = [-0.1 , 0.2, 0.2]
-bias = -0.2
-dataset = np.array([
-    [1,0,0],
-    [1,0,1],
-    [1,1,0],
-    [1,1,1]
-])
-neural = Neural(weights, bias, dataset)
-print(neural.logit)
+In the example below we've created a simple `Logic And` using the Our simple class
 
-#Lets try it out
+```python
+>>> from neurallogic import Neural
+>>> bias = -0.2
+>>> weights = [-0.1 , 0.2, 0.2]
+>>> import numpy as np
+>>> dataset = np.array([
+...     [1,0,0],
+...     [1,0,1],
+...     [1,1,0],
+...     [1,1,1]
+... ])
+>>> neural = Neural(weights, bias, dataset)
+>>> neural.logit
+array([[0.42555748],
+       [0.47502081],
+       [0.47502081],
+       [0.52497919]])
+>>> neural.__converted__()
+array([[0.],
+       [0.],
+       [0.],
+       [1.]])
 ```
+
--- neurallogic.py
+++ neurallogic.py
@@ -1,19 +1,20 @@
 import numpy as np
 
 class Neural:
-    """Creates a Perceptron
+    """Creates a Perceptron then returns the result of the perceptron using the data
+    you declared
     
     Returns:
         [type] -- [description]
     """
 
     def __init__(self, weights, bias, x):
-        """[summary]
+        """Initializes the Neural Class
         
         Arguments:
-            weights {[type]} -- [description]
-            bias {[type]} -- [description]
-            x {[type]} -- [description]
+            weights {list} -- List of doubles containing all the weights
+            bias {double} -- Data Bias
+            x {np.array} -- Dataset
         """
         self.weights = np.array(list(weights), dtype=np.double).reshape(len(weights), 1)
         self.x = np.array(x)
@@ -22,30 +23,35 @@ class Neural:
         
 
     def perceptron(self):
-        """[summary]
+        """Builds the Perceptron
         
         Returns:
-            [type] -- [description]
+            np.array -- numpy array containing all the answers that the perceptron gave
         """
         model = self.x.dot(self.weights) + self.bias
         logit = 1/(1+np.exp(-model))
         return logit
 
     def __wshape__(self):
-        """[summary]
+        """Retrieves the shape of the weights
         
         Returns:
-            [type] -- [description]
+            tuple -- shape of the weights
         """
         return self.weights.shape
 
     def __xshape__(self):
-        """[summary]
+        """Retrieves the shape of the dataset
         
         Returns:
-            [type] -- [description]
+            tuple -- shape of the dataset
         """
         return self.x.shape
     
     def __converted__(self):
+        """Converts the output of the perceptron into a rounded version
+        
+        Returns:
+            numpy array -- array of the rounded up results of the perceptron
+        """
         return np.round(self.logit)
